0de06da138e4f0dcd5b1815e1213942716623bfd
Stuart Jones <stuart@savvywombat.com.au>
2020-09-01 10:47:32 +0930

    can drag points around

--- src/components/bezier-curve-tool.vue
+++ src/components/bezier-curve-tool.vue
@@ -2,10 +2,15 @@
     <div class="flex justify-center m-4">
         <canvas ref="canvas"
                 class="border"
-                width="400"
-                height="400"
+                :height="height"
+                :width="width"
+                @mousedown="pointerDown"
+                @touchstart="pointerDown"
                 @mouseup="pointerUp"
                 @touchend="pointerUp"
+                @mouseupout="pointerCancel"
+                @touchendout="pointerCancel"
+                @touchcancel="pointerCancel"
         />
     </div>
 </template>
@@ -17,13 +22,30 @@ export default {
     data() {
         return {
             canvas: null,
-            points: []
+            dragging: -1,
+            points: [],
+
+            height: 400,
+            width: 400
         };
     },
 
     methods: {
         addPoint(x, y) {
-            this.points.push({x, y});
+            return this.points.push({x, y}) - 1;
+        },
+
+        movePoint(p, x, y) {
+            this.points[p] = {x, y};
+        },
+
+        detectPoint(x, y) {
+            return this.points.findIndex(({x: px, y: py}) => {
+                return px - 3 <= x
+                    && px + 3 >= x
+                    && py - 3 <= y
+                    && py + 3 >= y;
+            });
         },
 
         drawPoint(x, y) {
@@ -32,11 +54,40 @@ export default {
             this.canvas.stroke();
         },
 
-        pointerUp(event) {
+        pointerCancel(event) {
+            this.dragging = -1;
+        },
+
+        pointerDown(event) {
             const {x, y} = getPointerXY(event, true);
 
-            this.addPoint(x, y);
-            this.drawPoint(x, y);
+            let p = -1;
+            if ((p = this.detectPoint(x, y)) >= 0) {
+                this.dragging = p;
+            } else {
+                this.dragging = this.addPoint(x, y);
+                this.drawPoint(x, y);
+            }
+        },
+
+        pointerUp(event) {
+            if (this.dragging >= 0) {
+                const {x, y} = getPointerXY(event, true);
+
+                this.movePoint(this.dragging, x, y);
+                this.refresh();
+
+                this.dragging = -1;
+            }
+        },
+
+        refresh() {
+            this.canvas.fillStyle = 'white';
+            this.canvas.fillRect(0, 0, this.width, this.height);
+
+            this.points.forEach(({x, y}) => {
+                 this.drawPoint(x, y);
+            });
         }
     },
 
