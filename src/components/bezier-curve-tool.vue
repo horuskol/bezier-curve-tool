e335cd624f221f1fef31165a9c45a50c9de47531
Stuart Jones <stuart@savvywombat.com.au>
2020-09-01 16:56:46 +0930

    draw a grid

--- src/components/bezier-curve-tool.vue
+++ src/components/bezier-curve-tool.vue
@@ -1,8 +1,8 @@
 /<template>
     <div class="flex justify-center m-4">
-        <div class="flex-shrink">
-            <div class="flex flex-wrap">
-                <h3 class="w-full flex-shrink-0">Mouse control</h3>
+        <div class="max-w-md flex-shrink">
+            <div class="flex flex-wrap justify-center">
+                <h3 class="w-full flex-shrink-0 text-center">Mouse control</h3>
 
                 <button class="block my-2 py-2 px-4 bg-white hover:bg-gray-100 disabled:bg-gray-400 text-gray-800 font-semibold border border-gray-400 rounded-l shadow"
                         type="button"
@@ -21,12 +21,46 @@
                 </button>
             </div>
 
+            <div class="flex flex-wrap gap-x-10 gap-y-3 mt-8 justify-center">
+                <h3 class="w-full flex-shrink-0 text-center">Grid options</h3>
+
+                <span class="w-1/3 text-center">
+                    <input id="showGrid" type="checkbox" :checked="showGrid" @change="showGrid = !showGrid"/>
+                    <label for="showGrid">Show grid</label>
+                </span>
+
+                <span class="w-1/3 text-center">
+                    <input id="snapToGrid" type="checkbox" :checked="snapToGrid" @change="snapToGrid = !snapToGrid"/>
+                    <label for="snapToGrid">Snap to grid</label>
+                </span>
+
+                <span class="w-1/3">
+                    <label for="gridX" class="block w-full text-center">Grid width</label>
+                    <input id="gridX"
+                           type="number"
+                           class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal text-right"
+                           :value="gridX"
+                           @change="(event) => { gridX = event.target.value }"
+                    />
+                </span>
+
+                <span class="w-1/3">
+                    <label for="gridY" class="block w-full text-center">Grid height</label>
+                    <input id="gridY"
+                           type="number"
+                           class="bg-white focus:outline-none focus:shadow-outline border border-gray-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal text-right"
+                           :value="gridY"
+                           @change="(event) => { gridY = event.target.value }"
+                    />
+                </span>
+            </div>
+
             <span v-if="currentPoint.x">x: {{ currentPoint.x }}</span>
             <span v-if="currentPoint.y">x: {{ currentPoint.y }}</span>
         </div>
 
         <canvas ref="canvas"
-                class="border"
+                class="border border-gray-900"
                 :height="height"
                 :width="width"
                 @mousedown="pointerDown"
@@ -47,11 +81,17 @@ import {getPointerXY} from 'lib/pointers';
 export default {
     data() {
         return {
+            buffer: null,
             canvas: null,
             dragging: -1,
             modifying: -1,
             mode: 'points',
             points: [],
+
+            gridX: 10,
+            gridY: 10,
+            showGrid: false,
+            snapToGrid: false,
 
             height: 400,
             width: 400
@@ -65,6 +105,12 @@ export default {
             }
 
             return {};
+        }
+    },
+
+    watch: {
+        showGrid(value) {
+            this.refresh();
         }
     },
 
@@ -88,14 +134,52 @@ export default {
             });
         },
 
+        async drawGrid() {
+            if (this.showGrid) {
+                const x = Number.parseInt(this.gridX);
+                const y = Number.parseInt(this.gridY);
+
+                this.buffer.canvas.height = x;
+                this.buffer.canvas.width = y;
+
+                this.buffer.fillStyle = 'white';
+                this.buffer.fillRect(0, 0, x, y);
+
+                this.buffer.strokeStyle = '#808080';
+                this.buffer.moveTo(0, 3);
+                this.buffer.lineTo(0, 0);
+                this.buffer.lineTo(3, 0);
+                this.buffer.moveTo(0, y - 2);
+                this.buffer.lineTo(0, y);
+                this.buffer.moveTo(x - 2, 0);
+                this.buffer.lineTo(x, 0);
+                this.buffer.stroke();
+
+                const image = new Image;
+
+                return new Promise((resolve, reject) => {
+                    image.onload = () => {
+                        this.canvas.fillStyle = this.canvas.createPattern(image, 'repeat');
+                        this.canvas.fillRect(0, 0, this.canvas.canvas.width, this.canvas.canvas.height);
+
+                        resolve();
+                    }
+                    image.onerror = reject;
+                    image.src = this.buffer.canvas.toDataURL();
+                });
+            }
+        },
+
         drawPath() {
-            this.canvas.strokeStyle = 'black';
-            this.canvas.beginPath();
-            this.canvas.moveTo(this.points[0].x, this.points[0].y);
-            this.points.forEach(({x, y}) => {
-                this.canvas.lineTo(x, y);
-            });
-            this.canvas.stroke();
+            if (this.points.length > 0) {
+                this.canvas.strokeStyle = 'black';
+                this.canvas.beginPath();
+                this.canvas.moveTo(this.points[0].x, this.points[0].y);
+                this.points.forEach(({x, y}) => {
+                    this.canvas.lineTo(x, y);
+                });
+                this.canvas.stroke();
+            }
         },
 
         drawPoint(p, x, y) {
@@ -139,18 +223,22 @@ export default {
             }
         },
 
-        refresh() {
+        async refresh() {
             this.canvas.fillStyle = 'white';
             this.canvas.fillRect(0, 0, this.width, this.height);
 
+            await this.drawGrid();
+
             this.points.forEach(({x, y}, p) => {
-                 this.drawPoint(p, x, y);
+                this.drawPoint(p, x, y);
             });
+
             this.drawPath();
         }
     },
 
     mounted() {
+        this.buffer = document.createElement('canvas').getContext('2d');
         this.canvas = this.$refs['canvas'].getContext('2d');
     }
 };
